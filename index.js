ad976169510c678822d22af01b07e0893d627899
Hernan Payrumani <hernan.payrumani@gmail.com>
2015-10-25 18:40:26 -0400

    Added two non zero with characters

---
+++ index.js
@@ -0,0 +1,15 @@
+modulo.exports = function () {
+  SPACE: '\u200b',
+  SPACE_HTML: '&#8203;',
+  NON_JOINER: '\u200c',
+  NON_JOINER_HTML_FIRST: '&#8204;',
+  NON_JOINER_HTML_SECOND: '&zwnj;',
+
+  function: isZeroWidth (string) {
+  
+  }
+
+  function: isZeroWidthHTML (string) {
+  
+  }
+}
